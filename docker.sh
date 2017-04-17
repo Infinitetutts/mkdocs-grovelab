48c929a073a2c2529a30cb5213703715f5695265
ryan-infinite <ryan.steenkamp@manalto.com>
2017-04-17 15:15:46 +0200

    first build

--- docker.sh
+++ docker.sh
@@ -2,13 +2,13 @@
 
 if [ "$1" == "start" ]
 then
-    docker run --rm -it -p 8000:8000 -v /Users/admin/Study/Docker/infinitetutts/mkdocs/volume/grovelab:/usr/share/mkdocs/grovelab infinitetutts/alpine-mkdocs:1.0
+    docker run --rm -it -p 8000:8000 -v /Users/admin/Study/Docker/infinitetutts/mkdocs-grovelab/volume/grovelab:/usr/share/mkdocs/grovelab infinitetutts/alpine-mkdocs:1.0
   elif [ "$1" == "build" ]
 then
-    docker run --rm -it -v /Users/admin/Study/Docker/infinitetutts/mkdocs/volume/grovelab:/usr/share/mkdocs/grovelab infinitetutts/alpine-mkdocs:1.0 build
+    docker run --rm -it -v /Users/admin/Study/Docker/infinitetutts/mkdocs-grovelab/volume/grovelab:/usr/share/mkdocs/grovelab infinitetutts/alpine-mkdocs:1.0 build
 elif [ "$1" == "gh-deploy" ]
 then
-    docker run --rm -it -v /Users/admin/Study/Docker/infinitetutts/mkdocs/volume/grovelab:/usr/share/mkdocs/grovelab infinitetutts/alpine-mkdocs:1.0 gh-deploy
+    docker run --rm -it -v /Users/admin/Study/Docker/infinitetutts/mkdocs-grovelab/volume/grovelab:/usr/share/mkdocs/grovelab infinitetutts/alpine-mkdocs:1.0 gh-deploy
 elif [ "$1" == "stop" ]
 then
     echo "Coming soon :P"
