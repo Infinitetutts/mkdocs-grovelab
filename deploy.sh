34c8101d29b1589d7176cb6b7cb0d00747bb65d9
Ryan Steenkamp <ryan.steenkamp@kurtosys.com>
2018-04-19 22:13:41 +0200

    Refactered

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-DEV_DIR=~/infinitetutts/projects/git/grovelab
-GROVELAB=~/infinitetutts/projects/git/mkdocs-grovelab
+
+PROD_DIR=
+DEV_DIR=
 
 bash $DEV_DIR/docker.sh build
-cp $GROVELAB/Readme.md /tmp/Readme.md && rm -rv $GROVELAB/* && cp /tmp/Readme.md $GROVELAB
-cp -rv $DEV_DIR/volume/grovelab/site/* $GROVELAB
-echo -e "\nDone ! Please do a git push\n"
+
+read -r -p "Are you sure that I may remove $PROD_DIR/* [Y/n]" response
+response=${response,,} # tolower
+  if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
+    cp $PROD_DIR/Readme.md /tmp/Readme.md && rm -rv $PROD_DIR/* && cp /tmp/Readme.md $PROD_DIR
+    cp -rv $DEV_DIR/volume/grovelab/site/* $PROD_DIR
+    echo -e "\nPlease do a git push <3 \n"
+  else
+    echo -e "\nDeploy failed\n"
+  fi
+
+
