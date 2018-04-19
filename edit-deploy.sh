7d88cb96d4e6ed4eddf8e86578e6b01affdf48de
Ryan Steenkamp <ryan.steenkamp@kurtosys.com>
2018-04-19 22:23:04 +0200

    Refractered

--- edit-deploy.sh
+++ edit-deploy.sh
@@ -8,11 +8,10 @@
 PROD_DIR=""
 DEV_DIR=""
 
-bash $DEV_DIR/docker.sh build
-
 read -r -p "Are you sure that I may remove $PROD_DIR/* [Y/n]" response
 response=${response,,} # tolower
   if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
+    bash $DEV_DIR/docker.sh build
     cp $PROD_DIR/Readme.md /tmp/Readme.md && rm -rv $PROD_DIR/* && cp /tmp/Readme.md $PROD_DIR
     cp -rv $DEV_DIR/volume/grovelab/site/* $PROD_DIR
     echo -e "\nPlease do a git push <3 \n"
